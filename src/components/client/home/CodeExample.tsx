9eff2569011c68feecd5fe995d934291b9db1777
MaxChamberlain <97307277+MaxChamberlain@users.noreply.github.com>
2023-07-03 09:50:34 -0700

    update title

--- src/components/client/home/CodeExample.tsx
+++ src/components/client/home/CodeExample.tsx
@@ -7,16 +7,8 @@ export default function CodeExample(){
     return(
         <div className='p-16 w-3/5 bg-white/5 rounded-xl backdrop-blur-3xl border border-white/10 flex flex-col gap-16 mt-32'>
             <span className='w-full text-center'>
-              CHOOSE PRODUCT
+              GET STARTED
             </span>
-            <div className='w-full flex justify-between bg-[#090619] rounded-full px-8 py-4 border border-white'>
-              <span>
-                DEDICATED DATACENTER PROXIES
-              </span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-              </svg>
-            </div>
             <EndpointExampleCode language={language} />
             <div className='w-full flex justify-center gap-4 text-center'>
               <div className={`bg-gradient-to-br from-white/10 to-white/20 hover:from-white/15 hover:to-white/25 rounded-full px-8 py-4 border ${language === 'python' ? 'border-blue-500' : 'border-white/20'} w-full cursor-pointer transition-colors duration-200`} onClick={() => {
